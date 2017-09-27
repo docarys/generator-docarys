908a5d6a5f0353c8be1fc84b63b123d7ccc00d62
Sergio Sisternes <sesispla@outlook.com>
2017-09-27 20:35:36 +0100

    Changed template .gitignore file name. Causing error when executing the generator outside the dev environment.

--- generators/app/index.js
+++ generators/app/index.js
@@ -49,7 +49,7 @@ module.exports = class extends Generator {
 
         if (this.ctx.git) {
             this.fs.copyTpl(
-                this.templatePath(".gitignore"),
+                this.templatePath("gitignore"),
                 this.destinationPath(".gitignore"),
                 this.ctx
         );
