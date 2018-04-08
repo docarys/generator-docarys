bf209925d970f1a6ae2377e9753e06f5d3484bb0
Sergio Sisternes <sesispla@outlook.com>
2018-04-08 13:47:45 +0200

    Updated tests

--- generators/app/index.test.js
+++ generators/app/index.test.js
@@ -25,9 +25,9 @@ describe("App", function () {
                 var cfg = yaml.load("docarys.yml");
                 expect(cfg).toExist();
                 expect(cfg.site_name).toBe("docarys");
-                expect(cfg.theme).toBe("material");
-                expect(cfg.extra.palette.primary).toBe("lime");
-                expect(cfg.extra.palette.accent).toBe("indigo");
+                expect(cfg.theme.name).toBe("material");
+                expect(cfg.theme.palette.primary).toBe("lime");
+                expect(cfg.theme.palette.accent).toBe("indigo");
                 expect(cfg.pages).toExist();
             });
     });
